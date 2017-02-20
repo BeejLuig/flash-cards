f57e347bedbccfc9405f837920fadcd9c15cadd1
BeejLuig <bjcantlupe@gmail.com>
2017-02-19 20:10:32 -0500

    Test pulling info from OAuth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    
+    @request = ''
+    if request.env['omniauth.auth']
+      @request = request.env['omniauth.auth']
+    end
   end
+
 end
