f3b419af8e584440b747859f08a8a7d3125de78f
BeejLuig <bjcantlupe@gmail.com>
2017-02-19 23:20:31 -0500

    Add jumbotron styling and remove unnecessary ApplicationController code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:uid])
-  end
-
   def index
     @user = User.find_by_id(session[:id])
   end
