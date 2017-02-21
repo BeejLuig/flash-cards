9a0fdbdbbe5769a4927a9d0d650d4a196227dcaa
BeejLuig <bjcantlupe@gmail.com>
2017-02-21 10:54:45 -0500

    Create Folders controller and routes

---
+++ app/controllers/folders_controller.rb
@@ -0,0 +1,19 @@
+class FoldersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :study_sets
+  resources :folders, only: [:new, :create, :edit, :update, :delete, :show]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
