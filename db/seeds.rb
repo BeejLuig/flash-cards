9ee158e5fa15ec29ee8226f270800d5aa5003d48
BeejLuig <bjcantlupe@gmail.com>
2017-02-28 10:01:28 -0500

    fix seed data. Add flash card count to user show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ end
 
 @user_2 = User.create(email: "test2@test.com", password: "password")
 
-@study_set_1 = @user_2.study_sets.create(title: "Numbers in Spanish", description: "Numbers...in Spanish")
+@study_set_1 = @user_2.study_sets.create(title: "Numbers in Spanish", description: "Numbers...in Spanish", owner: @user_2)
 
 @study_set_1.flash_cards.create(term: "Uno",definition: "One")
 @study_set_1.flash_cards.create(term: "Dos",definition: "Two")
@@ -32,7 +32,7 @@ end
 @study_set_1.flash_cards.create(term: "Nueve",definition: "Nine")
 @study_set_1.flash_cards.create(term: "Diez",definition: "Ten")
 
-@study_set_2 = @user_2.study_sets.create(title: "Distance from the Sun",description: "Distance of planets in our solar system to the sun, in Astronomical Units")
+@study_set_2 = @user_2.study_sets.create(title: "Distance from the Sun",description: "Distance of planets in our solar system to the sun, in Astronomical Units", owner: @user_2)
 @study_set_2.flash_cards.create(term: "Mercury", definition: "0.4 AU")
 @study_set_2.flash_cards.create(term: "Venus", definition: "0.7 AU")
 @study_set_2.flash_cards.create(term: "Earth", definition: "1.0 AU")
@@ -43,7 +43,7 @@ end
 @study_set_2.flash_cards.create(term: "Neptune", definition: "30.2 AU")
 @study_set_2.flash_cards.create(term: "Pluto", definition: "39.5 AU")
 
-@study_set_3 = @user_2.study_sets.create(title: "Common Literary Elements",description: "Literary terms and definitions")
+@study_set_3 = @user_2.study_sets.create(title: "Common Literary Elements",description: "Literary terms and definitions", owner: @user_2)
 @study_set_3.flash_cards.create(term: "Plot", definition: "The logical sequence of events that develops a story")
 @study_set_3.flash_cards.create(term: "Setting", definition: "Refers to the time and place in which a story takes place")
 @study_set_3.flash_cards.create(term: "Protagonist", definition: "The main character of a story, novel or play")
@@ -57,7 +57,7 @@ end
 
 @user_3 = User.create(email: "test3@test.com", password: "password")
 
-@study_set_1 = @user_3.study_sets.create(title: "Musical Terms", description: "Italian terms seen in written music")
+@study_set_1 = @user_3.study_sets.create(title: "Musical Terms", description: "Italian terms seen in written music", owner: @user_3)
 @study_set_1.flash_cards.create(term: "A cappella",definition: "One or more vocalists performing without an accompaniment")
 @study_set_1.flash_cards.create(term: "Cadenza",definition: "Improvised or written out passage for a soloist")
 @study_set_1.flash_cards.create(term: "Da capo",definition: "An instruction to repeat the beginning of the piece before stopping on the final chord")
@@ -71,7 +71,7 @@ end
 @study_set_1.flash_cards.create(term: "Presto",definition:"Instruction to play at a fast tempo")
 @study_set_1.flash_cards.create(term: "Largo",definition: "Instruction to play at a slow tempo")
 
-@study_set_2 = @user_3.study_sets.create(title: "Presidents Term Years",description: "Term years of famous US presidents")
+@study_set_2 = @user_3.study_sets.create(title: "Presidents Term Years",description: "Term years of famous US presidents", owner: @user_3)
 
 @study_set_2.flash_cards.create(term: "George Washington",definition: "1789-1797")
 @study_set_2.flash_cards.create(term: "Thomas Jefferson",definition: "1797-1801")
@@ -90,7 +90,7 @@ end
 @study_set_2.flash_cards.create(term: "George H.W. Bush",definition:"1989-1993")
 @study_set_2.flash_cards.create(term: "William Jefferson Clinton",definition:"1993-2001")
 
-@study_set_3 = @user_3.study_sets.create(title: "Android Version Code Names",description:"Nicknames used for major Android releases")
+@study_set_3 = @user_3.study_sets.create(title: "Android Version Code Names",description:"Nicknames used for major Android releases", owner: @user_3)
 
 @study_set_3.flash_cards.create(term: "Android 1.5",definition:"Cupcake")
 @study_set_3.flash_cards.create(term: "Android 1.6",definition:"Donut")
