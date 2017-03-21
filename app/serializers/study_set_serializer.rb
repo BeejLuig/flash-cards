4144125db21d667929e9b156d5bb8b05366bb0aa
BeejLuig <bjcantlupe@gmail.com>
2017-03-21 19:18:09 -0400

    fix study_set/owner association in serializers

--- app/serializers/study_set_serializer.rb
+++ app/serializers/study_set_serializer.rb
@@ -1,4 +1,5 @@
 class StudySetSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :owner_id
+  attributes :id, :title, :description
   has_many :flash_cards
+  belongs_to :owner
 end
