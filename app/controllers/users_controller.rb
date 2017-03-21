f3173404d08c4733b51b4fd583bd11407fab8f87
BeejLuig <bjcantlupe@gmail.com>
2017-03-21 17:53:06 -0400

    Fix number of "study sets" on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    @flash_cards = @user.flash_cards.select { |fc| fc.study_set.owner == @user}
     @study_sets = @user.study_sets.select { |set| set.owner == @user }
     @studied = @user.study_sets.select {|set| set.owner != @user }
     @folders = @user.folders
