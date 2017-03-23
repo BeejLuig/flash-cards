a5e92a4a4825cd50d75e22fda2864687ecdd83b6
BeejLuig <bjcantlupe@gmail.com>
2017-03-23 18:37:06 -0400

    set up handlebars template for study set search results, add some placeholder code

--- app/assets/javascripts/study_sets.js
+++ app/assets/javascripts/study_sets.js
@@ -1,16 +1,36 @@
 class StudySet {
-  constructor(title, description, ownerEmail, flashCards = []){
+  constructor(title, description, owner, flashCards = []){
     this.title = title;
     this.description = description;
-    this.ownerEmail = ownerEmail;
+    this.ownerId = owner["id"];
+    this.ownerImage = owner["image"]
+    this.ownerEmail = owner["email"];
     this.flashCards = flashCards;
+  }
+
+  flashCardCount() {
+    return this.flashCards.length
   }
 }
 
+function transformStudySets(studySets) {
+  sets = [];
+  studySets.forEach(function(set){
+    sets.push(new StudySet(set["title"], set["description"], set["owner"], set["flash_cards"]))
+  });
+  return sets;
+}
+
 var attachListeners = function(){
-  searchStudySetHandler();
 }
 
 $(function(){
-  attachListeners();
+  attachListeners()
+  $.get("/study_sets.json", function(sets) {
+    var data = { studySets: transformStudySets(sets)}
+    console.log(data);
+    var source = $("#studySet-template").html();
+    var template = Handlebars.compile(source);
+    $("#searchResults").html(template(data))
+  });
 })
