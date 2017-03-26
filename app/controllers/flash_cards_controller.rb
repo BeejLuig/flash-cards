e1ee6e898d0cf717226d094aa0d95b3d03d2850a
BeejLuig <bjcantlupe@gmail.com>
2017-03-25 23:05:22 -0400

    add strong params for flash_cards#create

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -5,6 +5,12 @@ class FlashCardsController < ApplicationController
   end
 
   def create
+    binding.pry
+  end
 
+  private
+
+  def flash_card_params
+    params.require(:flash_card).permit(:term, :definition)
   end
 end
