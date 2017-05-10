220a5d935fc32f00363f2a73280d18741188b4ee
BeejLuig <bjcantlupe@gmail.com>
2017-05-09 21:46:50 -0400

    Add browserify-rails and package.json

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+#Use browserify-rails for deployment
+gem "browserify-rails"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module FlashCards
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
   end
 end
